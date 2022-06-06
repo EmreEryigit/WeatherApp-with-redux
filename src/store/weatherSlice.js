6013ee78335cda8132dc68808c1b6ab61af71289
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-06-06 22:43:11 +0300

    Kurulum

---
+++ src/store/weatherSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const weatherSlice = createSlice({
+    name: "weather",
+    initialState: {
+
+    },
+    reducers: {
+
+    }
+})
+
+export default weatherSlice
+export const weatherActions = weatherSlice.actions
